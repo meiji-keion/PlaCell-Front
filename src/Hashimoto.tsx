3d904fe2bbaad4b17277ca5c64cfea86c3f841f4
HashimotoShuhei-z <hassii0510@outlook.jp>
2024-08-16 20:46:47 +0900

    chakra uiをインストール

---
+++ src/Hashimoto.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hashimoto = () => {
+  return (
+    <div>
+      <h1>ytytっytっっytyty</h1>
+      <button>橋本</button>
+      <button>よろしくお願いします</button>
+      <button>よろしくお願ふあファファsファsf</button>
+    </div>
+  )
+}
+export default Hashimoto
