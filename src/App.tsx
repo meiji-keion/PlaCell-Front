722f5665e07e0d547491663191f56737a60945e9
yuufksw <yuufksw1009@gmail.com>
2024-08-17 00:57:51 +0900

    desktop2 作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,12 @@ import React from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Hashimoto from './Hashimoto'
+import LoginPage from './LoginPage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>プラセル</p>
-        <Hashimoto />
-      </header>
+    <div>
+      <LoginPage/>
     </div>
   )
 }
