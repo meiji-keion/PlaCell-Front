413839e9e50862a316891f6a191e60f65207d248
hannahvivi <hannahrachel622@icloud.com>
2024-08-25 17:51:37 +0900

    top-pageの作成

--- src/TopPage.tsx
+++ src/TopPage.tsx
@@ -7,10 +7,13 @@ const TopPage = () => {
   return (
     <div>
       <Center mt={20}>
-      <Heading color='purple.700'>PlaCell</Heading>
+      <Heading color='purple.700' fontSize='6xl'>PlaCell</Heading>
+      </Center>
+      <Center mt={5}>
+      <Heading color='purple.300' fontSize='xl'>時間単位で日程調整できる予定決めアプリ</Heading>
       </Center>
       <Center mt={8}>
-       <Button colorScheme='purple.500' variant='outline' mr={4}>編集</Button> 
+       <Button colorScheme='purple' variant='outline' mr={4}>編集</Button> 
        <Button colorScheme='blue'variant='outline' ml={4}>作成</Button> 
      </Center>
      
@@ -19,3 +22,4 @@ const TopPage = () => {
 }
 
 export default TopPage
+/*説明図都下分とか書き込むかもだけど一旦*/ 
