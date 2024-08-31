c1f06d13beb7a481ace06a2990c96b5f0b3dcbd8
hannahvivi <hannahrachel622@icloud.com>
2024-09-01 01:23:26 +0900

    desktop9の作成

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,9 +3,9 @@ import LoginPage from "../components/pages/LoginPage";
 import RegisterPage from "../components/pages/RegisterPage";
 import EditLoginPage from "../components/pages/EditLoginPage";
 import TopPage from "../components/pages/TopPage";
-import Hyou from "../components/partsGroups/Timetable";
 import Event from "../components/pages/desktop4";
 import Edit from "../components/pages/desktop7";
+import Enter from "../components/pages/desktop9";
 
 export const Router =()=>{
     return(
@@ -14,7 +14,7 @@ export const Router =()=>{
         <Route path ="/login" element={<LoginPage />} />
         <Route path ="/register" element={<RegisterPage />} />
         <Route path ="/editlogin" element={<EditLoginPage />} />
-        <Route path ="/plan" element={<Hyou />} />
+        <Route path ="/enter" element={<Enter />} />
         <Route path ="/event" element={<Event />} />
         <Route path ="/edit" element={<Edit />} />
     </Routes>
